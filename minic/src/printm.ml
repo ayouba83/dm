e7f8e91b2787a26209e3a8e87646ecc427c8b47a
ayouba83 <ayoubadeba83@gmail.com>
2021-12-20 23:33:13 +0100

    ajustage des décalages dans l'afficheur

--- minic/src/printm.ml
+++ minic/src/printm.ml
@@ -10,14 +10,74 @@ let print (programme: prog) =
     done;
   in
   (* affiche un type *)
-  let typ_to_str = function
+  let rec typ_to_str = function
     | Int  -> "int" 
     | Bool -> "bool"
     | Void -> "void"
+    | Tab(t) -> (typ_to_str t)
+    | Ptr(t) -> (typ_to_str t)^"*"
   in
-  (*affichage des variables globales du programme*)
-  List.iter (fun (x, _, v) -> Printf.printf " int %s = %n;\n" x v) programme.globals;
+
+  (* affiche une expression *)
+  let rec affiche_expr = function
+  | Cst n -> Printf.printf "%n" n
+  | BCst b -> Printf.printf "%b" b
+  | Array (a) -> Printf.printf "{"; affiche_params_eff (Array.to_list a); Printf.printf "}"
+  | Get var -> Printf.printf "%s" var
+  | Par e -> Printf.printf "("; affiche_expr e; Printf.printf ")"
+  | Call(f, params) -> begin
+                          Printf.printf "%s(" f;
+                          affiche_params_eff params;
+                          Printf.printf ")";
+                        end
+ (* opérateurs arithemétiques *)
+  | Add(e1, e2) -> affiche_expr e1; Printf.printf " + "; affiche_expr e2
+  | Sub(e1, e2) -> affiche_expr e1; Printf.printf " - "; affiche_expr e2
+  | Mul(e1, e2) -> affiche_expr e1; Printf.printf " * "; affiche_expr e2
+  | Div(e1, e2) -> affiche_expr e1; Printf.printf " / "; affiche_expr e2
+  | Opp(e) ->  Printf.printf "-"; affiche_expr e
+ (* opérateurs de comparaison *)
+  | Lt(e1, e2) -> affiche_expr e1; Printf.printf " < "; affiche_expr e2
+  | Gt(e1, e2) -> affiche_expr e1; Printf.printf " > "; affiche_expr e2
+  | Eq(e1, e2) -> affiche_expr e1; Printf.printf " == "; affiche_expr e2
+  | Ne(e1, e2) -> affiche_expr e1; Printf.printf " != "; affiche_expr e2
+  | Le(e1, e2) -> affiche_expr e1; Printf.printf " <= "; affiche_expr e2
+  | Ge(e1, e2) -> affiche_expr e1; Printf.printf " >= "; affiche_expr e2
+ (* opérateurs logiques (booléens) *)
+  | Not(e) -> Printf.printf "!"; affiche_expr e
+  | Andl(e1, e2) -> affiche_expr e1; Printf.printf " && "; affiche_expr e2
+  | Orl(e1, e2) -> affiche_expr e1; Printf.printf " || "; affiche_expr e2
+ (* sucres sytaxiques *)
+  | Incr(x) -> Printf.printf "%s++" x
+  | Decr(x) -> Printf.printf "%s--" x
+and affiche_params_eff = function
+  |[] -> Printf.printf ""
+  |[e] -> affiche_expr e
+  |e::fin -> affiche_expr e; Printf.printf ", "; affiche_params_eff fin
+in
   
+  (*affichage des variables globales du programme*)
+  
+  let affiche_var_init (x, t, v) =
+    begin match t with
+    |Int |Bool |Void |Ptr(_) -> Printf.printf "%s %s" (typ_to_str t) x; Printf.printf "= "; affiche_expr v;
+    |Tab(_) -> Printf.printf "%s %s" (typ_to_str t) x; Printf.printf "[]"; Printf.printf "= "; affiche_expr v;
+    end
+  in 
+  let affiche_var_not_init (x, t) =
+    begin match t with
+    |Int |Bool |Void |Ptr(_) -> Printf.printf "%s %s" (typ_to_str t) x;
+    |Tab(_) -> Printf.printf "%s %s" (typ_to_str t) x; Printf.printf "[]";
+    end
+  in
+   let affiche_var (x, t, v) =
+    begin match v with
+    | None -> affiche_var_not_init (x, t)
+    | Some(va) -> affiche_var_init (x, t, va)
+      end
+  in
+  List.iter (fun x -> Printf.printf " "; affiche_var x; Printf.printf ";\n") programme.globals;
+
   (* affichage des fonctions du programme *)
   let affiche_fun (def: fun_def) =
     (*afficher <type> <nom> (<paramètres>) { *)
@@ -25,51 +85,14 @@ let print (programme: prog) =
     let rec affiche_params (l: (string * typ) list) =
       match l with
       |[] -> Printf.printf ""
-      |[(x, t)] -> Printf.printf "%s %s" (typ_to_str t) x
-      |(x,t)::fin -> Printf.printf "%s %s, " (typ_to_str t) x; affiche_params fin
+      |[(x, t)] -> affiche_var (x, t, None)
+      |(x,t)::fin -> affiche_var (x, t, None); Printf.printf ", "; affiche_params fin
     in
     affiche_params def.params;
     Printf.printf "){\n";
 
     (* afficher les variables locales *)
-    List.iter (fun (x, t) -> decal !level; Printf.printf "%s %s;\n" (typ_to_str t) x) def.locals;
-
-    (* affiche une expression *)
-    let rec affiche_expr = function
-      | Cst n -> Printf.printf "%n" n
-      | BCst b -> Printf.printf "%b" b
-      | Get var -> Printf.printf "%s" var
-      | Par e -> Printf.printf "("; affiche_expr e; Printf.printf ")"
-      | Call(f, params) -> begin
-                              Printf.printf "%s(" f;
-                              affiche_params_eff params;
-                              Printf.printf ")";
-                            end
-     (* opérateurs arithemétiques *)
-      | Add(e1, e2) -> affiche_expr e1; Printf.printf " + "; affiche_expr e2
-      | Sub(e1, e2) -> affiche_expr e1; Printf.printf " - "; affiche_expr e2
-      | Mul(e1, e2) -> affiche_expr e1; Printf.printf " * "; affiche_expr e2
-      | Div(e1, e2) -> affiche_expr e1; Printf.printf " / "; affiche_expr e2
-      | Opp(e) ->  Printf.printf "-"; affiche_expr e
-     (* opérateurs de comparaison *)
-      | Lt(e1, e2) -> affiche_expr e1; Printf.printf " < "; affiche_expr e2
-      | Gt(e1, e2) -> affiche_expr e1; Printf.printf " > "; affiche_expr e2
-      | Eq(e1, e2) -> affiche_expr e1; Printf.printf " == "; affiche_expr e2
-      | Ne(e1, e2) -> affiche_expr e1; Printf.printf " != "; affiche_expr e2
-      | Le(e1, e2) -> affiche_expr e1; Printf.printf " <= "; affiche_expr e2
-      | Ge(e1, e2) -> affiche_expr e1; Printf.printf " >= "; affiche_expr e2
-     (* opérateurs logiques (booléens) *)
-      | Not(e) -> Printf.printf "!"; affiche_expr e
-      | Andl(e1, e2) -> affiche_expr e1; Printf.printf " && "; affiche_expr e2
-      | Orl(e1, e2) -> affiche_expr e1; Printf.printf " || "; affiche_expr e2
-     (* sucres sytaxiques *)
-      | Incr(x) -> Printf.printf "%s++" x
-      | Decr(x) -> Printf.printf "%s--" x
-    and affiche_params_eff = function
-      |[] -> Printf.printf ""
-      |[e] -> affiche_expr e
-      |e::fin -> affiche_expr e; Printf.printf ", "; affiche_params_eff fin
-    in
+    List.iter (fun x -> decal !level; affiche_var x; Printf.printf ";\n") def.locals;
 
     let affiche_instr_form = function
     | Putchar(e) ->  Printf.printf "putchar("; affiche_expr e; Printf.printf ")"
